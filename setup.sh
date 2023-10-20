7a13e0659418363ada4cd277014f4b4e6379c13c
fiqifalahdev <fiqifalah17@gmail.com>
2023-10-20 14:33:47 +0700

    new commend

--- setup.sh
+++ setup.sh
@@ -1,33 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-# Check if a single argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <repository/image:old_tag>"
-    exit 1
+if [ $# -ne 1 ]; then 
+	echo "Usage $0 <current_tag>"
+	echo exit 1
 fi
-# OK
 
-# Get the provided image reference
-old_image="$1"
+current_tag="$1"
 
 # Extract the registry, repository, and old tag
-registry=$(echo "$old_image" | cut -d/ -f1)
-repository=$(echo "$old_image" | cut -d/ -f2 | cut -d: -f1)
-old_tag=$(echo "$old_image" | cut -d: -f2)
-
-# Get the latest tag from the Docker registry
-latest_tag=$(docker pull --quiet "$old_image" | awk '/Digest:/ {print $2}' | cut -d: -f2)
-
-# Check if the latest tag is empty or the same as the old tag
-if [ -z "$latest_tag" ] || [ "$latest_tag" == "$old_tag" ]; then
-    echo "No new update found."
-    exit 0
-fi
-
-# Increment the version number (e.g., 1.0.0 to 1.0.1)
-new_tag=$(awk -F. -v OFS=. '{$NF++; print}')
+new_tag=$(echo "$current_tag" | cut -d: -f1)
+incremented_tag=$(echo "$new_tag", | awk -F. '{print $NF+1}')
+new_tag="1.0.$incremented_tag"
 
 # Create the new image reference with the incremented tag
-new_image="$registry/$repository:$new_tag"
+new_image="fiqifalah17/portofolio:$new_tag"
 
-echo "New image reference: $new_image"
+# echo "Pushing $new_image"
+echo "$new_image"
