fc9b056e6153cb1088948f37e4226778d3de23a3
fiqifalahdev <fiqifalah17@gmail.com>
2023-10-20 14:14:54 +0700

    change permission and shebang

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/bin/sh
 
 if [ $# -ne 1 ]; then 
 	echo "Usage $0 <current_tag>"
@@ -8,7 +8,9 @@ fi
 current_tag="$1"
 
 # Extract the registry, repository, and old tag
-new_tag=$(echo "$current_tag" | cut -d- -f1)
+new_tag=$(echo "$current_tag" | cut -d: -f1)
+incremented_tag=$(echo "$new_tag", | awk -F. '{print $NF+1}')
+new_tag="1.0.$incremented_tag"
 
 # Create the new image reference with the incremented tag
 new_image="fiqifalah/portofolio:$new_tag"
