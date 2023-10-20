7b62bf3d4d48ca50cf1ca0769eb6e5d6e97bcc5f
fiqifalahdev <fiqifalah17@gmail.com>
2023-10-20 14:29:41 +0700

    new command

--- setup.sh
+++ setup.sh
@@ -1,21 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-# check jika tidak ada parameter
-if [ $# -ne 1 ]; then 
-	echo "Usage $0 <current_tag>"
-	echo exit 1
+# Check if a single argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <repository/image:old_tag>"
+    exit 1
 fi
 
-# ambil parameter
-current_tag="$1"
+# Get the provided image reference
+old_image="$1"
 
-# Extract tag dari parameter
-new_tag=$(echo "$current_tag" | cut -d: -f1)
-incremented_tag=$(echo "$new_tag", | awk -F. '{print $NF+1}')
-new_tag="1.0.$incremented_tag"
+# Extract the registry, repository, and old tag
+registry=$(echo "$old_image" | cut -d/ -f1)
+repository=$(echo "$old_image" | cut -d/ -f2 | cut -d: -f1)
+old_tag=$(echo "$old_image" | cut -d: -f2)
 
-# buat image baru dari tag yang sudah di increment
-new_image="fiqifalah17/portofolio:$new_tag"
+# Get the latest tag from the Docker registry
+latest_tag=$(docker pull --quiet "$old_image" | awk '/Digest:/ {print $2}' | cut -d: -f2)
 
-# echo "Pushing $new_image"
-echo "$new_image"
+# Check if the latest tag is empty or the same as the old tag
+if [ -z "$latest_tag" ] || [ "$latest_tag" == "$old_tag" ]; then
+    echo "No new update found."
+    exit 0
+fi
+
+# Increment the version number (e.g., 1.0.0 to 1.0.1)
+new_tag=$(awk -F. -v OFS=. '{$NF++; print}')
+
+# Create the new image reference with the incremented tag
+new_image="$registry/$repository:$new_tag"
+
+echo "New image reference: $new_image"
