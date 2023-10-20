e3eac3c5a188dbbd8d7dc3cbd47c81565d97cecb
fiqifalahdev <fiqifalah17@gmail.com>
2023-10-20 16:15:11 +0700

    deploy

---
+++ cd.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+image="fiqi:1.0.1";
+
+echo $image;
--- setup.sh
+++ setup.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then 
-	echo "Usage $0 <current_tag>"
+	echo "Usage $0 <version>"
 	echo exit 1
 fi
 
-current_tag="$1"
+version="$1"
 
-# Extract the registry, repository, and old tag
-new_tag=$(echo "$current_tag" | cut -d: -f1)
-incremented_tag=$(echo "$new_tag", | awk -F. '{print $NF+1}')
-new_tag="1.0.$incremented_tag"
+# search=$(cat ./docker-compose.yml | grep portofolio:)
+search=$(cat ./docker-compose.yml | grep 'portofolio:' | sed 's/.*portofolio:[[:space:]]*//')
 
-# Create the new image reference with the incremented tag
-new_image="fiqifalah17/portofolio:$new_tag"
+sed -i "s/$search/$version/g" docker-compose.yml
 
-# echo "Pushing $new_image"
-echo "$new_image"
+docker pull fiqifalah17/portofolio:$version
+
+docker compose up -d
