9d0b1f8106afc6384e91033e84cb923fd39ee049
fiqifalahdev <fiqifalah17@gmail.com>
2023-10-20 14:23:27 +0700

    add comments

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+# check jika tidak ada parameter
 if [ $# -ne 1 ]; then 
 	echo "Usage $0 <current_tag>"
 	echo exit 1
 fi
 
+# ambil parameter
 current_tag="$1"
 
-# Extract the registry, repository, and old tag
+# Extract tag dari parameter
 new_tag=$(echo "$current_tag" | cut -d: -f1)
 incremented_tag=$(echo "$new_tag", | awk -F. '{print $NF+1}')
 new_tag="1.0.$incremented_tag"
 
-# Create the new image reference with the incremented tag
+# buat image baru dari tag yang sudah di increment
 new_image="fiqifalah17/portofolio:$new_tag"
 
 # echo "Pushing $new_image"
