ae00f3e3a51f510e021a24d2213382fbd4ce5bae
fiqifalahdev <fiqifalah17@gmail.com>
2023-10-23 13:05:00 +0700

    add directory path 2

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ version="$1"
 # search=$(cat ./docker-compose.yml | grep portofolio:)
 search=$(cat /root/My-Portofolio/docker-compose.yml | grep 'portofolio:' | sed 's/.*portofolio:[[:space:]]*//')
 
-sed -i "s/$search/$version/g" docker-compose.yml
+sed -i "s/$search/$version/g" /root/My-Portofolio/docker-compose.yml
 
 docker pull fiqifalah17/portofolio:$version
 
